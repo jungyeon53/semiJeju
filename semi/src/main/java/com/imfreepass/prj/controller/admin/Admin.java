e07aad2f55beacd2f9d62f25f3eb1a51d47d3bba
jungyeon <wjddus5533@naver.com>
2024-01-02 15:26:35 +0900

    test

--- semi/src/main/java/com/imfreepass/prj/controller/admin/Admin.java
+++ semi/src/main/java/com/imfreepass/prj/controller/admin/Admin.java
@@ -17,7 +17,7 @@ import com.imfreepass.prj.util.WebUtils;
 public class Admin extends HttpServlet{
 	private BoardService boardService = BoardService.getInstance();
 
-	// get
+	// get dd 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		MemberVO member = WebUtils.getMember(req);
